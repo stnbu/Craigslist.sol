287904f115580e0bffd6d6e731c7aea017b2d063
Mike Burr <mb@unintuitive.org>
2021-11-29 12:09:12 -0700

    another test

--- tests/test_.py
+++ tests/test_.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import pytest
+import brownie
 from brownie import Sale, accounts
 
 @pytest.fixture
@@ -11,3 +12,10 @@ def test_initial_happiness(sale):
     # Bug ID 0 -- why do these fail?
     assert sale.buyer_happy() == True
     assert sale.seller_happy() == True
+
+def test_blind_call_to_accept(sale):
+    with brownie.reverts():
+        # This call should revert because
+        #   1. state!=STARTED
+        #   2. account[0] is not "seller_address"
+        sale.acceptCurrentOffer()
