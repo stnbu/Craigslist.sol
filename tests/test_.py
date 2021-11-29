b7ea1b0eb93ed2a94713b210c6ae5ed8714c5791
Mike Burr <mb@unintuitive.org>
2021-11-29 13:47:15 -0700

    .

--- tests/test_.py
+++ tests/test_.py
@@ -14,7 +14,7 @@ FINALIZED = 3
 def fhex(n):
     return '0x' + n.hex()
 
-@pytest.fixture
+@pytest.fixture(scope="module", autouse=True)
 def params():
     # MAGIC HACK: these are "injected" via a globals update.
     testing_variables = {
@@ -29,21 +29,21 @@ def params():
     }
     globals().update(testing_variables)
 
-def test_constructor(params):
+def test_constructor():
     assert sale_contract.buyer_happy() == True
     assert sale_contract.seller_happy() == True
     assert sale_contract.state() == DEPLOYED
 
-def test_blind_call_to_accept(params):
+def test_blind_call_to_accept():
     assert sale_contract.state() == DEPLOYED
-    for wallet in [deployer, seller, buyer]:
+    for wallet in [deployer, seller, buyer]:  # Try three wallets, because.
         # This call should revert because
         #   1. state!=STARTED
         #   2. seller_address is uninitialized
         with brownie.reverts():
             sale_contract.acceptCurrentOffer({'from': wallet})
 
-def test_start(params):
+def test_start():
     sale_contract.startSale(sale_hash, seller, {'from': buyer})
     assert sale_contract.sale_hash() == fhex(sale_hash)
     assert sale_contract.seller_address() == seller.address
