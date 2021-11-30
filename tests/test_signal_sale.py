a10480fef3fbd8c4ac2657564a26261b7f98d08d
Mike Burr <mb@unintuitive.org>
2021-11-30 15:33:08 -0700

    .

--- tests/test_signal_sale.py
+++ tests/test_signal_sale.py
@@ -17,7 +17,9 @@ def fhex(n):
 
 @pytest.fixture
 def params():
-    # MAGIC HACK: these are "injected" via a globals update.
+    send_to_start = 2  # yes, two Wei. Is there an argument to use "realistic
+                       # amounts of money"?
+    # NOTE MAGIC HACK: these are "injected" via a globals update.
     testing_variables = {
         'deployer': accounts[0],
         'buyer': accounts[1],
@@ -26,87 +28,52 @@ def params():
         'sale_hash': (b'f\xd0Y\xea\x1e\x9b5\x10\xfcV\xa0'
                       b'\xba\xa4\x15\xd7\x0e\r\xb0g\xde'
                       b'\x13%\x84v\xfe\xe6(\xa5\xf9\x94\xd5\r'),
-        'initial_offer': Wei('0.001 ether'),
+        'initial_send': send_to_start,
+        'initial_offer': send_to_start / 2,
+        'initial_deposit': send_to_start / 2,
     }
     globals().update(testing_variables)
 
 @pytest.fixture
 def deployed(params):
-    testing_variables = {'sale_contract': accounts[0].deploy(SignalSale)}
+    # We get a new contract instance for each test using this harness. Same below.
+    testing_variables = {'sale_contract': deployer.deploy(SignalSale)}
     globals().update(testing_variables)
 
 @pytest.fixture
 def started(params):
-    testing_variables = {'sale_contract': accounts[0].deploy(SignalSale)}
+    testing_variables = {'sale_contract': deployer.deploy(SignalSale)}
     globals().update(testing_variables)
-    sale_contract.startSale(sale_hash, seller, {'from': buyer, 'value': initial_offer})
+    sale_contract.start(sale_hash, seller, initial_deposit, {'from': buyer, 'value': initial_send})
 
 @pytest.fixture
 def accepted(params):
-    testing_variables = {'sale_contract': accounts[0].deploy(SignalSale)}
+    testing_variables = {'sale_contract': deployer.deploy(SignalSale)}
     globals().update(testing_variables)
+    # FIXME: broken, obviously.
     sale_contract.startSale(sale_hash, seller, {'from': buyer})
     sale_contract.acceptCurrentOffer({'from': seller})
 
 def test_constructor(deployed):
-    assert sale_contract.buyer_happy() == True
-    assert sale_contract.seller_happy() == True
+    assert sale_contract.seller_address() == '0x' + '0' * 40
+    assert sale_contract.buyer_address() == '0x' + '0' * 40
     assert sale_contract.state() == DEPLOYED
+    assert sale_contract.offer() == 0;
+    assert sale_contract.buyer_deposit() == 0;
+    assert sale_contract.seller_deposit() == 0;
 
 def test_blind_call_to_accept(deployed):
-    assert sale_contract.state() == DEPLOYED
-    for wallet in [deployer, seller, buyer]:  # Try three wallets, because.
-        # This should revert because
-        #   1. state!=STARTED
-        #   2. seller_address is uninitialized
-        with brownie.reverts():
-            sale_contract.acceptCurrentOffer({'from': wallet})
+    # This should revert because
+    #   1. state!=STARTED
+    #   2. seller_address is uninitialized
+    with brownie.reverts():
+        sale_contract.accept({'from': seller, 'value': initial_offer})
 
 def test_started_state(started):
     assert sale_contract.sale_hash() == fhex(sale_hash)
     assert sale_contract.seller_address() == seller.address
     assert sale_contract.buyer_address() == buyer.address
     assert sale_contract.state() == STARTED
-
-def test_accept(started):
-    # This should revert. We are in the right `state` but the buyer
-    # should not be able to accept.
-    with brownie.reverts():
-        sale_contract.acceptCurrentOffer({'from': buyer})
-    sale_contract.acceptCurrentOffer({'from': seller})
-    assert sale_contract.state() == ACCEPTED
-    # The state is now ACCEPTED: no one, including the seller should be able
-    # to reject.
-    with brownie.reverts():
-        sale_contract.reject(True, {'from': seller})
-
-def test_reject_seller(started):
-    buyer_balance_start = buyer.balance()
-    # The seller rejects this STARTED sale with happy=False
-    sale_contract.reject(False, {'from': seller})
-    assert sale_contract.seller_happy() == False
-    assert sale_contract.buyer_happy() == True
-    assert buyer.balance() - buyer_balance_start == initial_offer
-
-def test_reject_buyer(started):
-    buyer_balance_start = buyer.balance()
-    # The buyer rejects this STARTED sale with happy=False
-    sale_contract.reject(False, {'from': buyer})
-    assert sale_contract.seller_happy() == True
-    assert sale_contract.buyer_happy() == False
-    # FIXME: shouldn't this fail? Shouldn't it be `initial_offer + gas_fees`?
-    assert buyer.balance() - buyer_balance_start == initial_offer
-
-def test_increment(started):
-    # The seller cannot increment
-    with brownie.reverts():
-        sale_contract.incrementOffer({'from': seller})
-    assert sale_contract.balance() == initial_offer
-    increment = Wei('0.0001 ether')
-    sale_contract.incrementOffer({'from': buyer, 'value': increment})
-    assert sale_contract.balance() == initial_offer + increment
-
-def test_finalize(accepted):
-    sale_contract.finalize(False, {'from': buyer})
-    assert sale_contract.seller_happy() == True
-    assert sale_contract.buyer_happy() == False
+    assert sale_contract.offer() == initial_offer
+    assert sale_contract.seller_deposit() == 0
+    assert sale_contract.buyer_deposit() == initial_deposit
