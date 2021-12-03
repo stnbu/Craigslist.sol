c93725f27ec69f79cb0465f6adfb239ee887da3a
Mike Burr <mb@unintuitive.org>
2021-12-03 14:06:46 -0700

    .

--- contracts/Testing.sol
+++ contracts/Testing.sol
@@ -2,11 +2,6 @@ pragma solidity ^0.6.0;
 
 contract SolidityHelpers {
 
-    uint foo = 0;
-
-    constructor() public {
-	foo = 2;
-    }
     // This lets our test automation get a calculated "signal_hash" from
     // solidity logic. Deferring how to do this correctly in python.
     function getSignalHash(bytes32 salt, uint signal, bool happy) public returns (bytes32) {
